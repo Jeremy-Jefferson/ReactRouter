6c4484c5b2d8aa28689c3863cf2d700c47954278
jeremy-jefferson <jeremyjefferson34@gmail.com>
2023-08-12 07:00:52 -0500

    y

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+        </Routes>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
